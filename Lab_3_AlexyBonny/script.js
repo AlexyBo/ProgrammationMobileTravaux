6414250f3d141cf3afc9be4597b159ff6ef87849
AlexyBo <alexy-bonny@hotmail.fr>
2018-02-09 07:32:32 -0500

    lab 3

---
+++ Lab_3_AlexyBonny/script.js
@@ -0,0 +1,26 @@
+function ajouterContenu(message)
+{
+	document.getElementById("resultat").innerHTML += message + "<br/>";
+}
+
+function viderContenu()
+{
+	document.getElementById("resultat").innerHTML = "";
+}
+
+function Exercice1()
+{
+	nombre=window.prompt("Entrez le numéro de la table:","Saisir un entier ici"); 
+
+var i; 
+for (i=1; i<=10; i++) 
+{ 
+document.write(nombre+" x "+i+"="+nombre*i+"<br>"); 
+} 
+}
+
+function Exercice2()
+{
+
+ 
+}
